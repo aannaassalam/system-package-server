edea66e04d8b9597ce2ff025969fd7a4f80c2062
Anas Alam <anasalam027@gmail.com>
2023-03-21 23:56:31 +0530

    limit changes in json & urlEncoded

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => res.send("Initial Route"));
 
